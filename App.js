02f93b9221d583e4663cd8fcd64ee9d3dfd0ca8c
Gimenes <f.gimenes1316@gmail.com>
2024-03-14 09:37:29 -0300

    all

--- App.js
+++ App.js
@@ -1,10 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
+
 
 export default function App() {
+  const [seconds, setSeconds] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [customInterval, setcustomInterval] = useState()
+
+  const startTimer = () => {
+    setcustomInterval(
+      setInterval(() => {
+        changeTime()
+      }, 1000)
+    );
+  }
+
+  const stopTimer = () => {
+    if (customInterval) {
+      clearInterval(customInterval);
+    }
+  }
+
+  const clear = () => {
+    stopTimer(0)
+    setMinutes(0)
+    setSeconds(0)
+  }
+
+  const changeTime = () => {
+    setSeconds((prevState) => {
+      if (prevState + 1 == 60) {
+        setMinutes(minutes + 1)
+        return 0
+      }
+      return prevState + 1
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        CRONOMETRO BACANA 
+      </Text>
+      <Image source={require("./assets/logo.png")} style={styles.logo}/>
+
+      <Text style={styles.textTimer}>
+        {minutes < 10 ? '0' + minutes : minutes}:
+        {seconds < 10 ? '0' + seconds : seconds}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button title='Start' onPress={startTimer} />
+        <Button title='Stop' onPress={stopTimer}/>
+        <Button title='Clear' onPress={clear}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +66,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textTimer: {
+    fontSize: 30,
+  },
+  buttonContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+  },
+  logo: {
+    marginBottom: 60,
+    marginTop: 60,
+
+  }
 });
